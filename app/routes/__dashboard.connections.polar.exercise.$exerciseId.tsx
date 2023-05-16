e3734c32e885ad7da23334103da5f2fc41d1aadb
Jonas Dahl <jonas@jdahl.se>
2023-05-16 18:08:01 +0200

    samples

--- app/routes/__dashboard.connections.polar.exercise.$exerciseId.tsx
+++ app/routes/__dashboard.connections.polar.exercise.$exerciseId.tsx
@@ -1,7 +1,22 @@
-import { Box, Code, Container, Heading, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Code,
+  Container,
+  Heading,
+  Stack,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { sum } from "lodash";
+import { Duration } from "luxon";
 import { ClientOnly } from "remix-utils";
 import { z } from "zod";
 import { authenticator } from "~/auth.server";
@@ -9,6 +24,7 @@ import {
   AnimatedAxis,
   AnimatedGrid,
   AnimatedLineSeries,
+  LineSeries,
   XYChart,
 } from "~/components/charts/xy-chart.client";
 import { db } from "~/db.server";
@@ -40,32 +56,115 @@ export async function loader({ request, params }: LoaderArgs) {
 
 export default function PolarExercise() {
   const { exercise, samples } = useLoaderData<typeof loader>();
+  const maxHeartRate = 197; // TODO
+  const heartRateSamples = samples
+    .filter((s) => s.sampleType === "0")
+    .flatMap((s) =>
+      s.samples.data.map((x) => {
+        const v = Number(x);
+        if (!v || isNaN(v)) {
+          return { value: null, duration: s.samples["recording-rate"] };
+        }
+        return { value: v, duration: s.samples["recording-rate"] };
+      })
+    );
+  const zones = [
+    { name: "Zon 1", max: 59.999 },
+    { name: "Zon 2", max: 75.999 },
+    { name: "Zon 3", max: 85.999 },
+    { name: "Zon 4", max: 93.999 },
+    { name: "Zon 5", max: 100 },
+  ].map(({ name, max }) => ({
+    name,
+    maxRelative: max,
+    max: maxHeartRate * (max / 100),
+  }));
+  const heartRateZones = zones.map((zone, i, arr) => ({
+    zone,
+    samples: heartRateSamples.filter(
+      (s) =>
+        s.value !== null &&
+        s.value <= zone.max &&
+        !arr.filter((_, j) => j < i).some((z) => s.value! <= z.max)
+    ),
+  }));
+
   return (
     <Container py={5} maxW="container.lg">
       <Stack>
         <Heading>Träning från Polar</Heading>
+        <Heading size="md">Puls</Heading>
+        <TableContainer>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>Zon</Th>
+                <Th>Puls (rel)</Th>
+                <Th>Puls (abs)</Th>
+                <Th>Tid i zon</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {heartRateZones.map(({ zone, samples }) => (
+                <Tr key={zone.name}>
+                  <Td w={1} whiteSpace="nowrap">
+                    {zone.name}
+                  </Td>
+                  <Td w={1} whiteSpace="nowrap">
+                    &lt;{zone.maxRelative.toFixed(0)}%
+                  </Td>
+                  <Td w={1} whiteSpace="nowrap">
+                    &lt;{zone.max.toFixed(0)}
+                  </Td>
+                  <Td>
+                    {Duration.fromMillis(
+                      sum(samples.map((s) => s.duration)) * 1000
+                    )
+                      .shiftTo("minutes", "seconds")
+                      .toFormat("m'min' ss's'")}
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
         <Box>
           <ClientOnly>
             {() => (
               <XYChart
                 height={300}
-                xScale={{ type: "band" }}
+                xScale={{ type: "linear" }}
                 yScale={{ type: "linear" }}
               >
                 <AnimatedAxis orientation="bottom" />
-                <AnimatedGrid columns={false} numTicks={4} />
-                {samples.map((sample) => (
-                  <AnimatedLineSeries
-                    key={sample.sampleType}
-                    dataKey={sample.sampleType}
-                    data={sample.samples.data.map((y, xDelta) => ({
-                      y,
-                      x: xDelta * sample.samples["recording-rate"],
-                    }))}
-                    xAccessor={(p) => p.x}
+                <AnimatedAxis orientation="left" />
+                <AnimatedGrid />
+                {zones.map((z) => (
+                  <LineSeries
+                    dataKey={z.name}
+                    key={z.name}
+                    data={[
+                      { x: 0, y: z.max },
+                      { x: 60, y: z.max },
+                    ]}
+                    xAccessor={(p) => p.x / 60}
                     yAccessor={(p) => p.y}
                   />
                 ))}
+                {samples
+                  .filter((s) => s.sampleType === "0")
+                  .map((sample) => (
+                    <AnimatedLineSeries
+                      key={sample.sampleType}
+                      dataKey={sample.sampleType}
+                      data={sample.samples.data.map((y, xDelta) => ({
+                        y: isNaN(Number(y)) || !Number(y) ? null : Number(y),
+                        x: xDelta * sample.samples["recording-rate"],
+                      }))}
+                      xAccessor={(p) => p.x / 60}
+                      yAccessor={(p) => p.y}
+                    />
+                  ))}
               </XYChart>
             )}
           </ClientOnly>
