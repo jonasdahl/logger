88cdb98deba65aa6f7e5937a8a7baa027b89776b
Jonas Dahl <jonas@jdahl.se>
2023-05-16 17:32:03 +0200

    add chart

--- app/routes/__dashboard.connections.polar.exercise.$exerciseId.tsx
+++ app/routes/__dashboard.connections.polar.exercise.$exerciseId.tsx
@@ -1,8 +1,16 @@
-import { Code, Container, Heading, Stack } from "@chakra-ui/react";
+import { Box, Code, Container, Heading, Stack } from "@chakra-ui/react";
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { ClientOnly } from "remix-utils";
+import { z } from "zod";
 import { authenticator } from "~/auth.server";
+import {
+  AnimatedAxis,
+  AnimatedGrid,
+  AnimatedLineSeries,
+  XYChart,
+} from "~/components/charts/xy-chart.client";
 import { db } from "~/db.server";
 
 export async function loader({ request, params }: LoaderArgs) {
@@ -14,21 +22,65 @@ export async function loader({ request, params }: LoaderArgs) {
     where: { userId: user.id, id: params.exerciseId },
   });
 
-  return json({ exercise });
+  return json({
+    exercise,
+    samples: z
+      .array(
+        z.object({
+          sampleType: z.string(),
+          samples: z.object({
+            "recording-rate": z.number(),
+            data: z.array(z.string()),
+          }),
+        })
+      )
+      .parse(JSON.parse(exercise.samples ?? "[]")),
+  });
 }
 
 export default function PolarExercise() {
-  const { exercise } = useLoaderData<typeof loader>();
+  const { exercise, samples } = useLoaderData<typeof loader>();
   return (
     <Container py={5} maxW="container.lg">
       <Stack>
         <Heading>Träning från Polar</Heading>
+        <Box>
+          <ClientOnly>
+            {() => (
+              <XYChart
+                height={300}
+                xScale={{ type: "band" }}
+                yScale={{ type: "linear" }}
+              >
+                <AnimatedAxis orientation="bottom" />
+                <AnimatedGrid columns={false} numTicks={4} />
+                {samples.map((sample) => (
+                  <AnimatedLineSeries
+                    key={sample.sampleType}
+                    dataKey={sample.sampleType}
+                    data={sample.samples.data.map((y, xDelta) => ({
+                      y,
+                      x: xDelta * sample.samples["recording-rate"],
+                    }))}
+                    xAccessor={(p) => p.x}
+                    yAccessor={(p) => p.y}
+                  />
+                ))}
+              </XYChart>
+            )}
+          </ClientOnly>
+        </Box>
+
         <Code as="pre" p={3} overflowX="auto">
           {JSON.stringify(exercise, null, 4)}
         </Code>
         <Heading>Raw</Heading>
         <Code as="pre" p={3} overflowX="auto">
           {JSON.stringify(JSON.parse(exercise.raw), null, 4)}
+        </Code>
+        <Heading>Samples</Heading>
+        <Code as="pre" p={3} overflowX="auto">
+          {JSON.stringify(samples, null, 4)}
         </Code>
       </Stack>
     </Container>
